9d87c811c0b6c8f86d94cff063684e1459a0cb4e
Torsten Spindler <torsten.spindler@mongodb.com>
2023-02-16 13:36:57 +0100

    Iterate over array comments

--- functions/verify_EDIT_support_casePortal.js
+++ functions/verify_EDIT_support_casePortal.js
@@ -1,8 +1,20 @@
 function verify(grant, targetRecord, proposedEdit) {
   grant.granted = true; //Allow the change
   grant.message = ''; //Use this to return an error to user
-  if (proposedEdit && proposedEdit.published == true && targetRecord.published == false) {
-    proposedEdit.date_published = new Date()
+
+  try {
+    if (proposedEdit && proposedEdit.comments && proposedEdit.comments.length > 0) {
+      for (var i = 0; i < proposedEdit.comments.length; i++) {
+        if (targetRecord.comments &&
+          targetRecord.comments[i] &&
+          proposedEdit.comments[i].published == true && targetRecord.comments[i].published == false) {
+          proposedEdit.comments[i].date_published = new Date()
+        }
+      }
+    }
+  } catch (e) {
+    grant.message = `${e}`
+    grant.granted = false // Fail on error
   }
 }
 
