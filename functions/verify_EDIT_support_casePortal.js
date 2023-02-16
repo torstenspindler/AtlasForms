49dd2230ad06c2ab3aaea60766359bb702d90118
Torsten Spindler <torsten.spindler@mongodb.com>
2023-02-16 12:56:31 +0100

    Use sample code from John

--- functions/verify_EDIT_support_casePortal.js
+++ functions/verify_EDIT_support_casePortal.js
@@ -1,22 +1,12 @@
-/* Duplicate this function to use it for your doctype */
-
-/* Functions named verify_ACTION_database_collection
-   Can be used to apply addidtional restrictions to an action
-   the gran object lets you deny the actions and add a message
-   you can also modify the arguments which vary by action */
-
-   function verify (grant, targetRecord, ...args) {
-    // You can assume yes and then deny or assume no then allow
-    // If you assume yes use a try/catch to ensure you don't allow
-    // on a bug.
-  
-    grant.granted = true
-    grant.message = 'Access granted'
-
+function verify(grant, targetRecord, proposedEdit) {
+  grant.granted = true; //Allow the change
+  grant.message = ''; //Use this to return an error to user
+  if (proposedEdit && proposedEdit.published == true && targetRecord.published == false) {
+    proposedEdit.date_published = new Date()
   }
-  
-  exports = function () {
-    // Return the function, don't call it.
-    return verify
-  }
-  
+}
+
+exports = function () {
+  // Don't call the function - just return it so we can call it by reference.
+  return verify
+}
