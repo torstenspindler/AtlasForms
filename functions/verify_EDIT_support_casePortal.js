8541e2a55869f2423bd93e5e03411602d054bf98
Torsten Spindler <torsten.spindler@mongodb.com>
2023-02-17 14:41:19 +0100

    Add preliminary logic on strings

--- functions/verify_EDIT_support_casePortal.js
+++ functions/verify_EDIT_support_casePortal.js
@@ -4,22 +4,24 @@ function verify(grant, targetRecord, proposedEdit) {
 
   try {
     console.log(`proposedEdit, stringified: ${EJSON.stringify(proposedEdit, null, 2)}`)
-    console.log(`proposedEdit: ${proposedEdit}`)
     console.log(`targetRecord: ${EJSON.stringify(targetRecord, null, 2)}`)
     // We ignore targetRecord, but we better fetch it and check that published was false for the comment, just in case
     // Also we override date_published if it was set before ...
     if (proposedEdit) {
-      console.log(`keys: ${Object.keys(proposedEdit)}`)
-      for (const key in Object.keys(proposedEdit)) {
-        console.log(`key: ${key}, value: ${proposedEdit[key]}`)
-        // if (proposedEdit.comments[key].published == true && proposedEdit.comments[key].date_published === undefined) {
-        //   console.log("Setting publish date")
-        //   proposedEdit.comments[key].date_published = new Date()
-        // }
-        // if (proposedEdit.comments[key].published == false && proposedEdit.comments[key].date_published !== undefined) {
-        //   console.log("Removing publish date")
-        //   delete proposedEdit.comments[key].date_published
-        // }
+      // This is a royal hackery, there has to be a better way!
+      for (const [key, value] of Object.entries(proposedEdit)) {
+        if (key.startsWith("comments.")) {
+          console.log(`key: ${key}, value: ${proposedEdit[key]}`)
+          if (key.endsWith(".published")) {
+          const date_publishedKey = key.replace(".published", ".date_published")
+            if (value == "true" && proposedEdit[date_publishedKey] == undefined) {
+              proposedEdit[date_publishedKey] = new Date()
+            }
+            if (value == "false" && proposedEdit[date_publishedKey]) {
+              delete proposedEdit[date_publishedKey]
+            }
+          }
+        }
       }
     }
   } catch (e) {
