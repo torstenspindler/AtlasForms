ef9739be126f3a2edeac080ce1f6297265bbed6e
Torsten Spindler <torsten.spindler@mongodb.com>
2023-02-17 14:41:19 +0100

    Add hopefully correct logic

--- functions/verify_EDIT_support_casePortal.js
+++ functions/verify_EDIT_support_casePortal.js
@@ -2,17 +2,20 @@ function verify(grant, targetRecord, proposedEdit) {
   grant.granted = true; //Allow the change
   grant.message = ''; //Use this to return an error to user
 
-  console.log("just a test")
-
   try {
     console.log(`proposedEdit: ${EJSON.stringify(proposedEdit, null, 2)}`)
     console.log(`targetRecord: ${EJSON.stringify(targetRecord, null, 2)}`)
-    if (proposedEdit && proposedEdit.comments && proposedEdit.comments.length > 0) {
-      for (var i = 0; i < proposedEdit.comments.length; i++) {
-        if (targetRecord.comments &&
-          targetRecord.comments[i] &&
-          proposedEdit.comments[i].published == true && targetRecord.comments[i].published == false) {
-          proposedEdit.comments[i].date_published = new Date()
+    // We ignore targetRecord, but we better fetch it and check that published was false for the comment, just in case
+    // Also we override date_published if it was set before ...
+    if (proposedEdit && proposedEdit.comments) {
+      for (const key in Object.keys(proposedEdit.comments)) {
+        if (proposedEdit.comments[key].published == true && proposedEdit.comments[key].date_published === undefined) {
+          console.log("Setting publish date")
+          proposedEdit.comments[key].date_published = new Date()
+        }
+        if (proposedEdit.comments[key].published == false && proposedEdit.comments[key].date_published !== undefined) {
+          console.log("Removing publish date")
+          delete proposedEdit.comments[key].date_published
         }
       }
     }
