87c7af43dec48e3e6d375e6a77aaec740480bcc4
Torsten Spindler <torsten.spindler@mongodb.com>
2023-02-17 14:38:22 +0100

    Iterating over keys in proposedEdit

--- functions/verify_EDIT_support_casePortal.js
+++ functions/verify_EDIT_support_casePortal.js
@@ -8,17 +8,17 @@ function verify(grant, targetRecord, proposedEdit) {
     console.log(`targetRecord: ${EJSON.stringify(targetRecord, null, 2)}`)
     // We ignore targetRecord, but we better fetch it and check that published was false for the comment, just in case
     // Also we override date_published if it was set before ...
-    if (proposedEdit && proposedEdit.comments) {
+    if (proposedEdit) {
       console.log(1)
-      for (const key in Object.keys(proposedEdit.comments)) {
+      for (const key in Object.keys(proposedEdit)) {
         console.log(`key: ${key}`)
-        if (proposedEdit.comments[key].published == true && proposedEdit.comments[key].date_published === undefined) {
+        if (proposedEdit[key].published == true && proposedEdit[key].date_published === undefined) {
           console.log("Setting publish date")
-          proposedEdit.comments[key].date_published = new Date()
+          proposedEdit[key].date_published = new Date()
         }
-        if (proposedEdit.comments[key].published == false && proposedEdit.comments[key].date_published !== undefined) {
+        if (proposedEdit[key].published == false && proposedEdit[key].date_published !== undefined) {
           console.log("Removing publish date")
-          delete proposedEdit.comments[key].date_published
+          delete proposedEdit[key].date_published
         }
       }
     }
